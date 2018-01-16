a442c3047f27fbdb964a2b2a58d694c006d846d5
PC <olechnowicz93@gmail.com>
2018-01-16 01:37:25 +0100

    setting up

---
+++ apteka-internetowa/index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<title>EFarma</title>
+<meta charset="UTF-8">
+<meta name="description" content="Przewodnik po kotach">
+<meta name="keywords" content="Apteka, Leki, Produkty farmakologiczne">
+<meta name="author" content="Andrzej Olechnowicz">
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<style type="text/css">
+</style>
+
+</head>
+
+<body>
+Tu będzie jakiś kurde kontent.
+</body>
+</html>
